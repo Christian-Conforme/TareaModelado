baa94dc8623258878c927f65b81810c3318c17ea
Yusley <yusleytp1@email.com>
2024-09-16 19:28:45 -0500

    Cambios

---
+++ tarea.js
@@ -0,0 +1,18 @@
+function roman(roman) {
+    if (roman=='I') {
+        return 1;
+    }else if (roman === 'V')
+        return 5;
+        if (roman === 'X') {
+            return 10;
+        } else if (roman === 'II')
+            return 2;
+       
+}
+
+
+console.log("el Número es",roman("I")); 
+console.log("el Número es",roman("V"));
+console.log("el Número es",roman("X"));
+console.log("el Número es",roman("II"));
+
